2608be5d6356542246b32702373faa9158e30fd7
Avinash Singh <avinash@dgist.in>
2019-11-27 21:45:02 +0530

    Handled on changed in 1.0.1

--- lib/flutter_typeahead.dart
+++ lib/flutter_typeahead.dart
@@ -291,6 +291,13 @@ class TypeAheadFormField<T> extends FormField<String> {
             builder: (FormFieldState<String> field) {
               final _TypeAheadFormFieldState state = field;
 
+              ValueChanged<String> origOnChange = textFieldConfiguration.onChanged;
+              ValueChanged<String> newOnChange = (v) {
+                if (origOnChange != null) {
+                  origOnChange(v);
+                }
+                state.didChange(v);
+              };
               return TypeAheadField(
                 getImmediateSuggestions: getImmediateSuggestions,
                 transitionBuilder: transitionBuilder,
@@ -302,7 +309,7 @@ class TypeAheadFormField<T> extends FormField<String> {
                 textFieldConfiguration: textFieldConfiguration.copyWith(
                   decoration: textFieldConfiguration.decoration
                       .copyWith(errorText: state.errorText),
-                  onChanged: state.didChange,
+                  onChanged: newOnChange,
                   controller: state._effectiveController,
                 ),
                 suggestionsBoxVerticalOffset: suggestionsBoxVerticalOffset,
